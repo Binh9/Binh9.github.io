4c3f94eaee96d452d5d628dbd5953694e048188a
Binh <lebinhb98@gmail.com>
2021-05-13 00:38:33 -0400

    completed homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div id='home' className='home'>
+      <div className='nav-bar'>
+        <a class='logo' href='#home'>BL</a>
+        <ul>
+          <li>
+            <a href='#aboutme'>About Me</a>
+          </li>
+          <li>
+            <a href='#experience'>Experience</a>
+          </li>
+          <li>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className='hey-message'>
+        <h2>Hey, I'm</h2>
+        <h1>Binh Le</h1>
+      </div> 
     </div>
   )
 }
