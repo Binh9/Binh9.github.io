fb8e8d83037072d718128d6fa9730755b673b4f9
Binh <lebinhb98@gmail.com>
2021-05-13 00:38:33 -0400

    completed homepage

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,5 @@
+import './css/fonts.css';
 import './App.css';
-import NavBar from './components/NavBar';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import Experience from './pages/Experience';
@@ -10,16 +9,13 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <>
-      <Router>
-        <NavBar/>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/aboutme' component={AboutMe} />
-          <Route path='/experience' component={Experience} />
-          <Route path='/projects' component={Projects} />
-          <Route path='/contact' component={Contact} />
-        </Switch>
-      </Router>
+      <div className={'mainContainer'}>
+        <Home/>
+        <AboutMe/>
+        <Experience/>
+        <Projects/>
+        <Contact/>
+      </div>
     </>
   );
 }
