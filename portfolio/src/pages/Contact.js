a456a3e2d4b81838a54a7461a1e5f670633ab2fb
Binh <lebinhb98@gmail.com>
2021-05-22 18:55:27 -0400

    wip

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 function Contact(props) {
   return (
-    <div id='contact' className='contact'>
-      <h1>Contact Me</h1>
+    <div id='contact' className='contact'> 
+      <div className='contactContainer'>
+        <h2>Contact</h2>  
+        <h2>I am happy to connect, feel free to reach me via</h2>
+        <div className='contactOptions'>
+          <div className='contactOption'>
+            LinkedIn
+          </div>
+          <div className='contactOption'>
+            Email 
+          </div>
+        </div>
+        <div className='backToTopBlock'>
+          <button>Back to Top</button>
+        </div>
+      </div>
     </div>
   );
 }
--- portfolio/src/pages/Experience.js
+++ portfolio/src/pages/Experience.js
@@ -1,9 +1,114 @@
 import React from 'react';
+import mmsLogo from './../images/mms.jpeg';
+import wayfairLogo from './../images/wayfair.jpeg';
+import khouryLogo from './../images/khoury.jpeg';
 
 function Experience(props) {
   return (
     <div id='experience' className='experience'>
-      <h1>Work Experience</h1>
+      <div className='experienceContainer'>
+        <div className='experienceHeader'>
+          My journey...
+        </div>
+        <div className='jobContainer'>
+          <div className='companyLogo'>
+            <img src={wayfairLogo} alt='wayfair logo' width={'100%'} height={'100%'} objectFit={'scale-down'}/>
+          </div>
+          <div className='jobDescription'>
+            <div className='companyOverview'>
+              <div className='companyName'>
+                <b>Wayfair</b>, Boston, MA
+              </div>
+              <div className='workDates'>
+                Jan. 2021 - Present
+              </div>
+            </div>
+            <div className='jobPosition'>
+              <i>Software Engineer Intern</i>
+            </div>
+            <div className='responsibilities'>
+              <ul>
+                <li className='jobBullets'>
+                  Build an integrated dashboard with Java and React that displays issues linked to the user leveraging third-party 
+                  and internal APIs
+                </li>
+                <li className='jobBullets'>
+                Collaborate with another engineer to develop a service informing thousands of employees in the Contact Centers and 
+                Warehouses of the ongoing incidents; thus, streamlining their workflow
+                </li>
+                <li className='jobBullets'>
+                  Coordinate with business user to migrate the Quality Assurance team from the in-house project tracking tool
+                </li>
+                <li className='jobBullets'>
+                  Maintain internal applications written in PHP and JavaScript
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='jobContainer'>
+          <div className='companyLogo'>
+            <img src={khouryLogo} alt='khoury logo' width={'100%'} height={'100%'}/>
+          </div>
+          <div className='jobDescription'>
+          <div className='companyOverview'>
+              <div className='companyName'>
+                Northeastern University, Boston, MA
+              </div>
+              <div className='workDates'>
+                Sep. 2020 - Dec. 2020
+              </div>
+            </div>
+            <div className='jobPosition'>
+              <i>Teaching Assistant for Discrete Structures</i>
+            </div>
+            <div className='responsibilities'>
+              <ul>
+                <li className='jobBullets'>
+                  Held weekly office hours to assist students with the course material and assignments
+                </li>
+                <li className='jobBullets'>
+                  Graded students’ homework and exam submissions
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='jobContainer'>
+          <div className='companyLogo'>
+            <img src={mmsLogo} alt='mms logo' width={'100%'} height={'100%'}/>
+          </div>
+          <div className='jobDescription'>
+          <div className='companyOverview'>
+              <div className='companyName'>
+                Massachusetts Medical Society, Waltham, MA
+              </div>
+              <div className='workDates'>
+                Jul. 2019 - Dec. 2019
+              </div>
+            </div>
+            <div className='jobPosition'>
+              <i>Quality Assurance Tester and Analyst</i>
+            </div>
+            <div className='responsibilities'>
+              <ul>
+                <li className='jobBullets'>
+                  Wrote REST API test scripts in Java based on the user stories using Selenium with Cucumber and executed the test 
+                  suits in Jenkins to verify the functionality of the company’s new unified commerce platform
+                </li>
+                <li className='jobBullets'>
+                  Coordinated work of the offshore team members and managed the sprint backlog of automation test 
+                  team to meet the deadline
+                </li>
+                <li className='jobBullets'>
+                  Performed manual functional testing for multiple website components and functions based on test cases and 
+                  specifications provided
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
