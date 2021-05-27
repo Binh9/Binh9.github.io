9fc4d010764056938a1cc688a31393b27036f83d
Binh <lebinhb98@gmail.com>
2021-05-26 23:18:59 -0400

    content finished

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import * as MdIcons from 'react-icons/md'
+import * as FaIcons from 'react-icons/fa'
 
 function Contact(props) {
   return (
     <div id='contact' className='contact'> 
       <div className='contactContainer'>
-        <h2>Contact</h2>  
-        <h2>I am happy to connect, feel free to reach me via</h2>
+        <div className='contactHeader'>
+          Contact...
+        </div>
+        <div className='contactSubheader'>
+          Happy to connect, feel free to reach me via
+        </div>
         <div className='contactOptions'>
           <div className='contactOption'>
+            <a href='https://www.linkedin.com/in/binhb998/' >
+              <FaIcons.FaLinkedin className='contactIcon' />
+            </a>
             LinkedIn
           </div>
           <div className='contactOption'>
-            Email 
+          <a href='mailto:lebinhb98@gmail.com' >
+              <MdIcons.MdEmail className='contactIcon' />
+            </a>
+            Email
           </div>
         </div>
         <div className='backToTopBlock'>
-          <button>Back to Top</button>
+          <a href='#home'><FaIcons.FaArrowCircleUp className='bttButton'/></a>
         </div>
       </div>
     </div>
